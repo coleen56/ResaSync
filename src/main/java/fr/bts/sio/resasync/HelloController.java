6ddbfd17de4c50e058c1bc3e503f636fe884bd4e
coleen56 <coleen.conte@orange.fr>
2025-04-09 13:49:13 +0200

    premier commit coleen

---
+++ src/main/java/fr/bts/sio/resasync/HelloController.java
@@ -0,0 +1,14 @@
+package fr.bts.sio.resasync;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module fr.bts.sio.resasync {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens fr.bts.sio.resasync to javafx.fxml;
+    exports fr.bts.sio.resasync;
+}
