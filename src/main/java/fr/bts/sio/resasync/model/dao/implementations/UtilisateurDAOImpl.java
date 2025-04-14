bd16f49fc108c0597e21f30e9e0a246c3bcee5c1
Flogte <florian.gerinte@gmail.com>
2025-04-14 14:16:46 +0200

    commit utilisateur DAO

--- src/main/java/fr/bts/sio/resasync/model/dao/implementations/UtilisateurDAOImpl.java
+++ src/main/java/fr/bts/sio/resasync/model/dao/implementations/UtilisateurDAOImpl.java
@@ -3,7 +3,11 @@ package fr.bts.sio.resasync.model.dao.implementations;
 import fr.bts.sio.resasync.model.dao.interfaces.UtilisateurDAO;
 import fr.bts.sio.resasync.model.entity.Utilisateur;
 
+import java.sql.Connection;
+
 public class UtilisateurDAOImpl implements UtilisateurDAO {
+    private Connection connection;
+
     @Override
     public Utilisateur findById(int idUtilisateur) {
         return null;
