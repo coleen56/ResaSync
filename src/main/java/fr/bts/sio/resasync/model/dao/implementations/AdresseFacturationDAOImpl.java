ab8243e9f317dc0f508219e5e5b3126a83a0640e
coleen56 <coleen.conte@orange.fr>
2025-04-14 11:27:35 +0200

    dao et dao impl entreprise et adressefacturation

--- src/main/java/fr/bts/sio/resasync/model/dao/implementations/AdresseFacturationDAOImpl.java
+++ src/main/java/fr/bts/sio/resasync/model/dao/implementations/AdresseFacturationDAOImpl.java
@@ -3,7 +3,15 @@ package fr.bts.sio.resasync.model.dao.implementations;
 import fr.bts.sio.resasync.model.dao.interfaces.AdresseFacturationDAO;
 import fr.bts.sio.resasync.model.entity.AdresseFacturation;
 
+import java.sql.Connection;
+
 public class AdresseFacturationDAOImpl implements AdresseFacturationDAO {
+    private Connection connection;
+
+    public AdresseFacturationDAOImpl(Connection connection) {
+        this.connection = connection;
+    }
+
     @Override
     public AdresseFacturation findById(int id) {
         return null;
