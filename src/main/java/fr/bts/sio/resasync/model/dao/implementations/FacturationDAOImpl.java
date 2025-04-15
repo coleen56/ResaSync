64f45d39fee2a70dc825ed6d40c5cf5bde4c7e91
coleen56 <coleen.conte@orange.fr>
2025-04-15 14:39:47 +0200

    dao et dao impl de facturation terminés

---
+++ src/main/java/fr/bts/sio/resasync/model/dao/implementations/FacturationDAOImpl.java
@@ -0,0 +1,4 @@
+package fr.bts.sio.resasync.model.dao.implementations;
+
+public class FacturationDAOImpl {
+}
---
+++ src/main/java/fr/bts/sio/resasync/model/dao/interfaces/FacturationDAO.java
@@ -0,0 +1,4 @@
+package fr.bts.sio.resasync.model.dao.interfaces;
+
+public interface FacturationDAO {
+}
