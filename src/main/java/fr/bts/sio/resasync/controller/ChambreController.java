7f90a743e5fedaa05a18f88a1b92d350046380d5
Flogte <florian.gerinte@gmail.com>
2025-04-30 16:36:18 +0200

    chambre visu ok

--- src/main/java/fr/bts/sio/resasync/controller/ChambreController.java
+++ src/main/java/fr/bts/sio/resasync/controller/ChambreController.java
@@ -1,16 +1,31 @@
 package fr.bts.sio.resasync.controller;
 
+import fr.bts.sio.resasync.model.dao.implementations.ChambreDAOImpl;
+import fr.bts.sio.resasync.model.dao.implementations.StatutChambreDAOImpl;
+import fr.bts.sio.resasync.model.dao.implementations.TypeChambreDAOImpl;
+import fr.bts.sio.resasync.model.dao.interfaces.ChambreDAO;
+import fr.bts.sio.resasync.model.dao.interfaces.StatutChambreDAO;
+import fr.bts.sio.resasync.model.dao.interfaces.TypeChambreDAO;
+import fr.bts.sio.resasync.model.entity.Chambre;
 import fr.bts.sio.resasync.model.entity.Session;
+import fr.bts.sio.resasync.model.entity.StatutChambre;
 import fr.bts.sio.resasync.util.Methods; // Import de la classe utilitaire Methods
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
-import javafx.scene.control.Hyperlink;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChambreController {
 
+    ChambreDAO chambreDAO = new ChambreDAOImpl();
+    TypeChambreDAO typeChambreDAO = new TypeChambreDAOImpl();
+    StatutChambreDAO statutChambre = new StatutChambreDAOImpl();
+
     @FXML private Hyperlink lienDashboard; // Lien pour retourner au Dashboard
-    @FXML private Hyperlink lienAjouterChambre; // Lien pour ajouter une chambre
-    @FXML private Hyperlink lienGestionReservations; // Lien pour gérer les réservations
 
     @FXML
     public void allerADashboard() {
@@ -34,7 +49,7 @@ public class ChambreController {
 
     @FXML
     public void Deconnexion() {
-        Methods.deconnexionAvecConfirmation(() -> chargerVue("Login.fxml")); // Appel direct à la méthode utilitaire
+        Methods.deconnexionAvecConfirmation(() ->chargerVue("Login.fxml")); // Appel direct à la méthode utilitaire
     }
 
     private void chargerVue(String fichierFxml) {
@@ -49,4 +64,53 @@ public class ChambreController {
             e.printStackTrace();
         }
     }
+
+    @FXML private TextField fieldNumChambre;
+    @FXML private ComboBox comboTypeChambre;
+
+
+
+    public void gererAjouterChambre(){
+        //Parse le retour de fieldNumChambre
+        int numero = Integer.parseInt(fieldNumChambre.getText());;
+        boolean existe = chambreDAO.findByNumeroChambre(numero);
+
+        if (existe) {
+            System.out.println("La chambre avec ce numéro existe déjà.");
+        } else {
+            System.out.println("La chambre n'existe pas.");
+        }
+        //TODO Vérifier que le numéro de la chambre n'existe pas déjà -> Créer un findByNumeroChambre
+        //Permet de retourner un chiffre entre 0 et 2
+        int type = (comboTypeChambre.getSelectionModel().getSelectedIndex())+1;
+
+        Chambre chambre = new Chambre(numero,type,1);
+        chambreDAO.ajouterChambre(chambre);
+        System.out.println("Chambre Ajoutée :");
+        System.out.println("NumChambre: " + chambre.getNumChambre());
+        System.out.println("IdTypeChambre: " + chambre.getIdTypeChambre());
+        System.out.println("IdStatutChambre: " + chambre.getIdStatutChambre());
+    }
+
+    @FXML private TableView<Chambre> tableChambres;
+    @FXML TableColumn<Chambre, Integer> colNumChambre;
+    @FXML TableColumn<Chambre, Integer> colTypeChambre;
+    @FXML TableColumn<Chambre, Integer> colStatut;
+    @FXML
+    public void initialize() {
+    colNumChambre.setCellValueFactory(new PropertyValueFactory<>("NumChambre"));
+    colTypeChambre.setCellValueFactory(new PropertyValueFactory<>("TypeChambreLibelle"));
+    colStatut.setCellValueFactory(new PropertyValueFactory<>("StatutChambreLibelle"));
+
+    afficherChambres();
+    }
+
+    public void afficherChambres(){
+        ArrayList<Chambre> chambres = chambreDAO.findAll();
+        tableChambres.setItems(FXCollections.observableArrayList(chambres));
+
+
+    }
+
+
 }
--- src/main/java/fr/bts/sio/resasync/model/dao/implementations/ChambreDAOImpl.java
+++ src/main/java/fr/bts/sio/resasync/model/dao/implementations/ChambreDAOImpl.java
@@ -8,50 +8,48 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class ChambreDAOImpl implements ChambreDAO {
     private Connection connection;
 
-    public ChambreDAOImpl(Connection connection) {
-        this.connection = connection;
-    }
 
     @Override
     public Chambre findById(int id) {
         String sql = "SELECT * FROM chambre WHERE idchambre = ?";
-        Chambre chambre = null;
+    Chambre chambre = null;
 
-        Connection conn = null;
-        PreparedStatement stmt = null;
+    Connection conn = null;
+    PreparedStatement stmt = null;
 
         try {
-            conn = DatabaseConnection.getConnection();
-            stmt = conn.prepareStatement(sql);
+        conn = DatabaseConnection.getConnection();
+        stmt = conn.prepareStatement(sql);
 
-            stmt.setInt(1, id);
-            ResultSet rs = stmt.executeQuery();
+        stmt.setInt(1, id);
+        ResultSet rs = stmt.executeQuery();
 
-            if (rs.next()) {
-                chambre = new Chambre(rs.getInt("idchambre"), rs.getInt("numchambre"),
-                        rs.getInt("idtypechambre"), rs.getInt("idstatutchambre"));
+        if (rs.next()) {
+            chambre = new Chambre(rs.getInt("idchambre"), rs.getInt("numchambre"),
+                    rs.getInt("idtypechambre"), rs.getInt("idstatutchambre"));
+        }
+    } catch (SQLException e) {
+        throw new RuntimeException(e);
+    } finally {
+        // Ferme les ressources
+        try {
+            if (stmt != null) {
+                stmt.close();
+            }
+            if (conn != null) {
+                conn.close();
             }
         } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } finally {
-            // Ferme les ressources
-            try {
-                if (stmt != null) {
-                    stmt.close();
-                }
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+            e.printStackTrace();
         }
-        return chambre;
     }
+        return chambre;
+}
 
     @Override
     public void save(Chambre chambre) {
@@ -153,4 +151,118 @@ public class ChambreDAOImpl implements ChambreDAO {
             }
         }
     }
+
+    public void ajouterChambre(Chambre chambre) {
+        String sql = "INSERT INTO chambre(numchambre, idtypechambre, idstatutchambre) VALUES (?, ?, ?)";
+        Connection conn = null;
+        PreparedStatement stmt = null;
+
+        try {
+            conn = DatabaseConnection.getConnection();
+            stmt = conn.prepareStatement(sql);
+
+            stmt.setInt(1, chambre.getNumChambre());
+            stmt.setInt(2, chambre.getIdTypeChambre());
+            stmt.setInt(3, chambre.getIdStatutChambre());
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            // Ferme les ressources
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public ArrayList<Chambre> findAll() {
+        String sql = "SELECT c.idchambre, c.numchambre, c.idtypechambre, c.idstatutchambre, s.libelle AS libelle_statut, t.libelle AS libelle_type "
+                + "FROM chambre c "
+                + "JOIN statutchambre s ON c.idstatutchambre = s.idstatutchambre "
+                + "JOIN typechambre t ON c.idtypechambre = t.idtypechambre";
+        ArrayList<Chambre> chambres = new ArrayList<>(); // Créer une ArrayList pour stocker les chambres
+        Connection conn = null;
+        PreparedStatement stmt = null;
+
+        try {
+            conn = DatabaseConnection.getConnection();
+            stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                // Récupérer le libellé du statut et du type de chambre
+
+                String libelleType = rs.getString("libelle_type");
+                String libelleStatut = rs.getString("libelle_statut");
+
+                // Créer un objet Chambre avec tous les attributs, y compris les libellés
+                Chambre chambre = new Chambre(
+                        rs.getInt("numchambre"),
+                        libelleType,
+                        libelleStatut// Libellé du type de chambre
+                );
+                chambres.add(chambre);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            // Ferme les ressources
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return chambres;
+    }
+
+    public boolean findByNumeroChambre(int numChambre){
+        String sql = "SELECT COUNT* FROM chambre WHERE numchambre = ?";
+        boolean chambreExiste = false ;
+        Connection conn = null;
+        PreparedStatement stmt = null;
+
+        try {
+            conn = DatabaseConnection.getConnection();
+            stmt = conn.prepareStatement(sql);
+
+            stmt.setInt(1, numChambre);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                int count = rs.getInt(1);
+                chambreExiste = count>0;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            // Ferme les ressources
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return chambreExiste;
+    }
 }
+
+
